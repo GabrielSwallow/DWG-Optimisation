3c7c031400aa8ce2acc32a9c587b5ca1ee4b2872
GabrielSwallow <gabrielswallow@googlemail.com>
2021-10-01 17:57:14 +0100

    cleaned up the file for easier referencing

--- API_example.py
+++ API_example.py
@@ -1,28 +1,25 @@
 import numpy as np
 import random as rd
-import time
 import matplotlib.pyplot as plt
 import plotparams
 
 #import julia, the api module
 import julia
+
+#Set the location for the julia excutable (if need be)
+julia.Julia(runtime="C:\\Users\\gabri\\AppData\\Local\\Programs\\Julia-1.5.4\\bin\\julia.exe")
+
 #julia.Main is used to add and access atributes to the julia namespace
 #inside Main.eval(), don't need Main.<atribute>, can just access via <attribute>
 from julia import Main
 
-#Julia.eval() runs julia code. Set as jl to simplify syntax
-#jl = julia.Julia(compiled_modules=False)
+#Main.eval() runs julia code.
+#Main.using("TextAnalysis") - imports Julia packages needed in .jl if need be
 
-
-#jl.using("TextAnalysis") - imports Julia packages needed in .jl if need be file
-
-
-#%%
 
 ####
 #Running julia in python directly
 ####
-#t1 = time.perf_counter()
 
 #define the data in python
 data1=[1,2,3,4,5]
@@ -32,16 +29,12 @@ Main.data_j1 = data1
 
 #define a julia function
 Main.eval('func(x) = x^2')
-#run and return the values back to a python object
 
+#run and return the values back to a python object
 out1 = Main.eval("func.(data_j1)")
 
-
-#t2 = time.perf_counter()
-#time_jl = t2-t1
-
-
-
+print("out1 = ", out1)
+print("type of out1 = ", type(out1))
 
 
 ####
@@ -53,6 +46,8 @@ Main.data_j2 = data2
 Main.eval('include("Playground.jl")')
 out2 = Main.eval("quad.(data_j2)")
 
+print("out2[0:10] = ", out2[0:10])
+print("type of out2 = ", type(out2))
 
 plt.plot(data2, out2)
 plt.ylim(0, 10100)
